ac3287eba9c82f2e6f2699d3a580b0765e643e46
LimDongsun <4restlim@gmail.com>
2021-11-30 18:28:46 +0900

    Config #1 - spring configuration
    기초 설정 
    루트 패키지 이하에 빈이 있는지 스캔한다

---
+++ src/main/java/com/practice/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.practice.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.practice.springpractice")
+@Configuration
+public class Config {
+}
