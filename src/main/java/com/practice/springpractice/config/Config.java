173cffe41bba6f00373a6963dbebf3b25f4efa90
LimDongsun <4restlim@gmail.com>
2021-12-03 18:34:15 +0900

    Implement #1 - WebApplicationInitializer
    기초 예제를 이용해 웹 mvc 진입점 코드와
    web mvc 기본 설정을 구현

--- src/main/java/com/practice/springpractice/config/Config.java
+++ src/main/java/com/practice/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.practice.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.practice.springpractice")
 @Configuration
 public class Config {
